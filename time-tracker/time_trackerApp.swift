2096671b0ea6ea4b2d7a827ed35925b2dd737728
lingyi tan <ltan@mathspace.com.au>
2023-12-02 19:09:10 +0100

    Initial Commit

---
+++ time-tracker/time_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  time_trackerApp.swift
+//  time-tracker
+//
+//  Created by lingyi on 2023-12-02.
+//
+
+import SwiftUI
+
+@main
+struct time_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
